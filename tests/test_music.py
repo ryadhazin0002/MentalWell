1a176508c056b4448c4b750a3e239ffeb1badbdd
Sergej <sergej.macut0023@stud.hkr.se>
2024-04-25 21:47:59 +0200

    Fixed tests

--- tests/test_music.py
+++ tests/test_music.py
@@ -9,6 +9,7 @@ class testMusic(unittest.TestCase):
         self.aandres = "..\\Music\\AAndres_Song.mp3"
         self.aerie = "..\\Music\\Aerie.mp3"
         self.cat = "..\\Music\\C418 - Cat - Minecraft Volume Alpha.mp3"
+        self.dog = "..\\Music\\C418 - Dog - Minecraft Volume Alpha.mp3"
         self.droopy = "..\\Music\\C418 - Droopy likes Ricochet - Minecraft Volume Alpha.mp3"
         self.dry_hands = "..\\Music\\C418 - Dry Hands - Minecraft Volume Alpha.mp3"
         self.haggstrom = "..\\Music\\C418 - Haggstrom - Minecraft Volume Alpha.mp3"
@@ -16,17 +17,18 @@ class testMusic(unittest.TestCase):
         self.mice_venus = "..\\Music\\C418 - Mice on Venus - Minecraft Volume Alpha.mp3"
         self.minecraft = "..\\Music\\C418 - Minecraft - Minecraft Volume Alpha.mp3"
         self.moog_city = "..\\Music\\C418 - Moog City - Minecraft Volume Alpha.mp3"
-        self.wet_hands = "..\\Music\\C418 - Wet Hands - Minecraft Volume Alpha.mp3"
-        self.dog = "..\\Music\\C418 - Dog - Minecraft Volume Alpha.mp3"
         self.sweden = "..\\Music\\C418 - Sweden - Minecraft Volume Alpha.mp3"
+        self.wet_hands = "..\\Music\\C418 - Wet Hands - Minecraft Volume Alpha.mp3"
         self.firebugs = "..\\Music\\Firebugs.mp3"
         self.moog_city_2 = "..\\Music\\Minecraft Volume Beta - Moog City 2.mp3"
+
 
     def teardown(self):
         self.music = src.music.Music()
         self.aandres = "..\\Music\\AAndres_Song.mp3"
         self.aerie = "..\\Music\\Aerie.mp3"
         self.cat = "..\\Music\\C418 - Cat - Minecraft Volume Alpha.mp3"
+        self.dog = "..\\Music\\C418 - Dog - Minecraft Volume Alpha.mp3"
         self.droopy = "..\\Music\\C418 - Droopy likes Ricochet - Minecraft Volume Alpha.mp3"
         self.dry_hands = "..\\Music\\C418 - Dry Hands - Minecraft Volume Alpha.mp3"
         self.haggstrom = "..\\Music\\C418 - Haggstrom - Minecraft Volume Alpha.mp3"
@@ -34,9 +36,8 @@ class testMusic(unittest.TestCase):
         self.mice_venus = "..\\Music\\C418 - Mice on Venus - Minecraft Volume Alpha.mp3"
         self.minecraft = "..\\Music\\C418 - Minecraft - Minecraft Volume Alpha.mp3"
         self.moog_city = "..\\Music\\C418 - Moog City - Minecraft Volume Alpha.mp3"
-        self.wet_hands = "..\\Music\\C418 - Wet Hands - Minecraft Volume Alpha.mp3"
-        self.dog = "..\\Music\\C418 - Dog - Minecraft Volume Alpha.mp3"
         self.sweden = "..\\Music\\C418 - Sweden - Minecraft Volume Alpha.mp3"
+        self.wet_hands = "..\\Music\\C418 - Wet Hands - Minecraft Volume Alpha.mp3"
         self.firebugs = "..\\Music\\Firebugs.mp3"
         self.moog_city_2 = "..\\Music\\Minecraft Volume Beta - Moog City 2.mp3"
 
@@ -48,7 +49,7 @@ class testMusic(unittest.TestCase):
         self.music.change_song(2)
         self.assertEqual(self.music.current_song, self.cat)
         self.music.change_song(3)
-        self.assertEqual(self.music.current_song, self.droopy)
+        self.assertEqual(self.music.current_song, self.dog)
 
     def test_next_song(self):
         self.music.change_song(-2)
@@ -61,13 +62,14 @@ class testMusic(unittest.TestCase):
         self.assertEqual(self.music.current_song, self.aerie)
 
     def test_prev_song(self):
+        self.music.change_song(0)
         self.assertEqual(self.music.current_song, self.aandres)
         self.music.prev_song()
         self.assertEqual(self.music.current_song, self.moog_city_2)
         self.music.prev_song()
         self.assertEqual(self.music.current_song, self.firebugs)
         self.music.prev_song()
-        self.assertEqual(self.music.current_song, self.sweden)
+        self.assertEqual(self.music.current_song, self.wet_hands)
 
 
 class TestMusicCollection(unittest.TestCase):
@@ -77,6 +79,7 @@ class TestMusicCollection(unittest.TestCase):
         self.aandres = "..\\Music\\AAndres_Song.mp3"
         self.aerie = "..\\Music\\Aerie.mp3"
         self.cat = "..\\Music\\C418 - Cat - Minecraft Volume Alpha.mp3"
+        self.dog = "..\\Music\\C418 - Dog - Minecraft Volume Alpha.mp3"
         self.droopy = "..\\Music\\C418 - Droopy likes Ricochet - Minecraft Volume Alpha.mp3"
         self.dry_hands = "..\\Music\\C418 - Dry Hands - Minecraft Volume Alpha.mp3"
         self.haggstrom = "..\\Music\\C418 - Haggstrom - Minecraft Volume Alpha.mp3"
@@ -84,9 +87,8 @@ class TestMusicCollection(unittest.TestCase):
         self.mice_venus = "..\\Music\\C418 - Mice on Venus - Minecraft Volume Alpha.mp3"
         self.minecraft = "..\\Music\\C418 - Minecraft - Minecraft Volume Alpha.mp3"
         self.moog_city = "..\\Music\\C418 - Moog City - Minecraft Volume Alpha.mp3"
-        self.wet_hands = "..\\Music\\C418 - Wet Hands - Minecraft Volume Alpha.mp3"
-        self.dog = "..\\Music\\C418 - Dog - Minecraft Volume Alpha.mp3"
         self.sweden = "..\\Music\\C418 - Sweden - Minecraft Volume Alpha.mp3"
+        self.wet_hands = "..\\Music\\C418 - Wet Hands - Minecraft Volume Alpha.mp3"
         self.firebugs = "..\\Music\\Firebugs.mp3"
         self.moog_city_2 = "..\\Music\\Minecraft Volume Beta - Moog City 2.mp3"
 
@@ -96,6 +98,7 @@ class TestMusicCollection(unittest.TestCase):
         self.aandres = "..\\Music\\AAndres_Song.mp3"
         self.aerie = "..\\Music\\Aerie.mp3"
         self.cat = "..\\Music\\C418 - Cat - Minecraft Volume Alpha.mp3"
+        self.dog = "..\\Music\\C418 - Dog - Minecraft Volume Alpha.mp3"
         self.droopy = "..\\Music\\C418 - Droopy likes Ricochet - Minecraft Volume Alpha.mp3"
         self.dry_hands = "..\\Music\\C418 - Dry Hands - Minecraft Volume Alpha.mp3"
         self.haggstrom = "..\\Music\\C418 - Haggstrom - Minecraft Volume Alpha.mp3"
@@ -103,9 +106,8 @@ class TestMusicCollection(unittest.TestCase):
         self.mice_venus = "..\\Music\\C418 - Mice on Venus - Minecraft Volume Alpha.mp3"
         self.minecraft = "..\\Music\\C418 - Minecraft - Minecraft Volume Alpha.mp3"
         self.moog_city = "..\\Music\\C418 - Moog City - Minecraft Volume Alpha.mp3"
-        self.wet_hands = "..\\Music\\C418 - Wet Hands - Minecraft Volume Alpha.mp3"
-        self.dog = "..\\Music\\C418 - Dog - Minecraft Volume Alpha.mp3"
         self.sweden = "..\\Music\\C418 - Sweden - Minecraft Volume Alpha.mp3"
+        self.wet_hands = "..\\Music\\C418 - Wet Hands - Minecraft Volume Alpha.mp3"
         self.firebugs = "..\\Music\\Firebugs.mp3"
         self.moog_city_2 = "..\\Music\\Minecraft Volume Beta - Moog City 2.mp3"
 
@@ -119,34 +121,34 @@ class TestMusicCollection(unittest.TestCase):
         self.assertEqual(self.musicPath[2], self.cat)
 
     def test_fourth_item(self):
-        self.assertEqual(self.musicPath[3], self.droopy)
+        self.assertEqual(self.musicPath[3], self.dog)
 
     def test_fifth_item(self):
-        self.assertEqual(self.musicPath[4], self.dry_hands)
+        self.assertEqual(self.musicPath[4], self.droopy)
 
     def test_sixth_item(self):
-        self.assertEqual(self.musicPath[5], self.haggstrom)
+        self.assertEqual(self.musicPath[5], self.dry_hands)
 
     def test_seventh_item(self):
-        self.assertEqual(self.musicPath[6], self.mice)
+        self.assertEqual(self.musicPath[6], self.haggstrom)
 
     def test_eighth_item(self):
-        self.assertEqual(self.musicPath[7], self.mice_venus)
+        self.assertEqual(self.musicPath[7], self.mice)
 
     def test_ninth_item(self):
-        self.assertEqual(self.musicPath[8], self.minecraft)
+        self.assertEqual(self.musicPath[8], self.mice_venus)
 
     def test_tenth_item(self):
-        self.assertEqual(self.musicPath[9], self.moog_city)
+        self.assertEqual(self.musicPath[9], self.minecraft)
 
     def test_eleventh_item(self):
-        self.assertEqual(self.musicPath[10], self.wet_hands)
+        self.assertEqual(self.musicPath[10], self.moog_city)
 
     def test_twelfth_item(self):
-        self.assertEqual(self.musicPath[11], self.dog)
+        self.assertEqual(self.musicPath[11], self.sweden)
 
     def test_thirteenth_item(self):
-        self.assertEqual(self.musicPath[12], self.sweden)
+        self.assertEqual(self.musicPath[12], self.wet_hands)
 
     def test_fourteenth_item(self):
         self.assertEqual(self.musicPath[13], self.firebugs)
