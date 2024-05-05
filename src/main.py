ffbe1ab849adf68566823ccb43c6004f094bfa71
fetyan78 <144726437+fetyan78@users.noreply.github.com>
2024-05-05 19:29:31 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-#ALi
+from PyQt5.QtWidgets import QApplication
+
+import sys
+from loginlogout import LoginLogic
+from login import Ui_Form
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QWidget
+
+def main():
+    app = QApplication(sys.argv)
+    login_page = LoginLogic()
+    login_page.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
