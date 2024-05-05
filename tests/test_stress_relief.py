eb4febcf5eaf8c7b53309a39a8e5e95a4e642f23
mustafa.hkr <mustafa.al-bayati0036@stud.hkr.se>
2024-05-05 16:07:00 +0200

    unittest done

--- tests/test_stress_relief.py
+++ tests/test_stress_relief.py
@@ -1,10 +1,34 @@
 import unittest
 
+from src.stress_relief import StressFunction, Stress
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def setUp(self):    # Create stress objects for testing
+        self.stress = StressFunction()
+        self.stress.stresses = [
+            Stress(1, "image1.jpg", "Discription 1"),
+            Stress(2, "image2.jpg", "Discription 2"),
+            Stress(3, "image3.jpg", "Discription 3"),
+            Stress(4, "image4.jpg", "Discription 4"),
+            Stress(5, "image4.jpg", "Discription 5")
+        ]
 
+    def test_current_stress(self):
+        # Test initial current stress
+        self.assertEqual(self.stress.current_stress(), self.stress.stresses[0])
+
+    def test_iterating_through_current_stress(self): # Test iterating through all stresses
+        current_stress = self.stress.current_stress()
+        for expected_stress in self.stress.stresses[1:]:
+            current_stress = self.stress.next_stress()
+            self.assertEqual(current_stress, expected_stress)
+        self.assertEqual(self.stress.next_stress(), None)  # Test reaching the end
+
+    def test_previous_stress(self):
+        # Move to the last stress
+        self.stress.set_current_stress(len(self.stress.stresses) - 1)
+        self.assertEqual(self.stress.previous_stress(), self.stress.stresses[-2])
 
 if __name__ == '__main__':
     unittest.main()
